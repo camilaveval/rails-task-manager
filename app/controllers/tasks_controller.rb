0df40fee0b8201ac102fdebcbd023f53f5cc1fd6
camila vera <camilapazvera@gmail.com>
2021-08-08 16:10:25 -0400

    edita y elimina tarea, pero no funciona

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,34 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
   def new
     @task = Task.new
   end
+
   def create
     @task = Task.new(task_params).save
     redirect_to tasks_path
   end
-  def index
-    @tasks = Task.all
-  end
-  def show
-    @tasks = Task.find(params[:id])
-  end
+
   def edit
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to task_path
   end
   private
 
